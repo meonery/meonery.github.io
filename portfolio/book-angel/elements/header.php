e7ff60c094e8dc31af9217fcfccbdc622d2cf55a
meonery <mayrieleerlgen.nery@gmail.com>
2021-07-20 21:35:20 +0800

    portfolio added

--- portfolio/book-angel/elements/header.php
+++ portfolio/book-angel/elements/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <?php
-            define('BASE_URL','https://meonery-portfolio.herokuapp.com/portfolio/book-angel/');
+            define('BASE_URL','http://meonery-portfolio.herokuapp.com/portfolio/book-angel/');
             // define('BASE_URL','http://localhost:8100/portfolio/book-angel/');
         ?>
         <meta charset="utf-8">
